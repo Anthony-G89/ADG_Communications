8752fc0211e694252413d3cb1dc285e6653cfc9f
Anthony Guerrero <adguerrero1935@gmail.com>
2021-08-25 14:13:27 -0400

    Working on landing page. Still not sure how I want the setup look

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,15 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./style.css";
+
 
 const HomePage = () => {
 
 
     return (
+        <>
+            <div className="homepageContainer">
+                <h1 className="companyQuote">Connecting your business to the world</h1>
+                <img className="homepageImg" src={process.env.PUBLIC_URL + "./assets/Background-homepage-img.jpg"} />
+            </div>
 
-        <div className="homepageContainer">
-            <h1 className="companyQuote">Connecting your business to the world</h1>
 
-        </div>
+        </>
 
     )
 };
