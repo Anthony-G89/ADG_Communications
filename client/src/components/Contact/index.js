8b3cac1a5c354a700ded6bbca295d6a57ed4d143
Anthony Guerrero <adguerrero1935@gmail.com>
2021-09-13 18:20:40 -0400

    Added Functions to clear the states after user submit a note

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./style.css";
 
 
@@ -8,8 +8,23 @@ const Contact = () => {
     const [subject, setSubject] = useState("");
     const [description, setDescription] = useState("");
 
-    function sumbitNote () {
-        alert("message submitted");
+
+
+    const textSubject = useRef();
+    const descriptionInput = useRef();
+
+    const clearSubject = () => (textSubject.current.value = "");
+    const clearDescription = () => (descriptionInput.current.value = "");
+
+    function sumbitNote() {
+
+        if (!subject || !description) {
+            alert("subject & description are empty. Please provide details.");
+        } else {
+            alert("Your message is submitted");
+            clearSubject();
+            clearDescription();
+        }
     };
 
 
@@ -18,10 +33,10 @@ const Contact = () => {
             <div className="formWrapper">
                 <form className="formContainer">
                     <label className="subjectLabel" htmlFor="subject" >Subject:</label><br />
-                    <input className="subjectInput" type="text" onChange={event => setSubject(event.target.value + console.log(event.target.value))} />
+                    <input className="subjectInput" ref={textSubject} type="text" onChange={event => setSubject(event.target.value)} />
 
                     <label className="DescriptionLabel" htmlFor="text">Description:</label><br />
-                    <textarea style={{ resize: "none" }} rows="12" name="userDescription" onChange={event => setDescription(event.target.value + console.log(event.target.value))} ></textarea>
+                    <textarea style={{ resize: "none" }} rows="12" name="userDescription" ref={descriptionInput} onChange={event => setDescription(event.target.value)} ></textarea>
                 </form>
                 <button onClick={sumbitNote} className="submitBtn">Submit</button>
             </div>
